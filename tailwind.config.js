ea3714313d1aa915e204c8b251b935fbea5d7f9c
Aamir <747.aamir@gmail.com>
2022-08-21 01:02:14 +0530

    added webfonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,16 @@ module.exports = {
       keyframes: {
         move: {
           '0%': { transform: 'translate(0px,0px)' },
-          '100%': { transform: 'translate(0px, -80%)'}
+          '100%': { transform: 'translate(0px, -80%)' }
         },
         move2: {
           '0%': { transform: 'translate(0px,-80%)' },
-          '100%': { transform: 'translate(0px, 0px)'}
+          '100%': { transform: 'translate(0px, 0px)' }
         }
-      }
+      },
+      fontFamily: {
+        fujimaru: ["Fujimaru"]
+      },
     },
   },
   plugins: [],
